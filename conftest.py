7f0f93005e02a778557d8857d2733728e692067b
mrKamanov <mr.kamanov@yandex.ru>
2025-02-02 15:38:14 +0300

    Add conftest.py with language fixture and browser setup

--- conftest.py
+++ conftest.py
@@ -1,17 +1,23 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-import pytest
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose language: en, ru, es, etc.")
 
-
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+def browser(request):
+    user_language = request.config.getoption("--language")
+
+    options = ChromeOptions()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    print(f"\nstart browser for test with language: {user_language}..")
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
     yield browser
     print("\nquit browser..")
     browser.quit()
