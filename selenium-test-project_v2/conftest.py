033c0aaa3fb7a00718abf3d884ea784b530fc7da
mrKamanov <mr.kamanov@yandex.ru>
2025-02-03 01:16:40 +0300

    "Добавлена фикстура browser для управления браузером и выбора языка интерфейса"

--- selenium-test-project_v2/conftest.py
+++ selenium-test-project_v2/conftest.py
@@ -5,19 +5,26 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose language: en, ru, es, etc.")
+    parser.addoption(
+        '--language',
+        action='store',
+        default='en',
+        help="Choose language: en, ru, es, etc."
+    )
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("--language")
-
     options = ChromeOptions()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-
+    options.add_experimental_option(
+        'prefs',
+        {'intl.accept_languages': user_language}
+    )
     print(f"\nstart browser for test with language: {user_language}..")
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-
+    browser = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=options
+    )
     yield browser
     print("\nquit browser..")
     browser.quit()
