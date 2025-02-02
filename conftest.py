077bb4f4750ab5b5eb9bc77df67392ee501dd817
mrKamanov <mr.kamanov@yandex.ru>
2025-02-02 14:50:33 +0300

    Add is_element_present method and improve error handling in tests

--- conftest.py
+++ conftest.py
@@ -1,11 +1,17 @@
-import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+import pytest
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose language: en, ru, es, etc.")
 
+
 @pytest.fixture(scope="function")
 def browser():
-    # Здесь можно будет добавить конфигурацию браузера
-    pass
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,8 @@
 import pytest
-from .pages.main_page import MainPage
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # Инициализируем Page Object
-    page.open()                    # Открываем страницу
-    page.go_to_login_page()        # Переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
